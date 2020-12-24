6da50d5364a2b5b5385d98d5c952a14bd2810155
Eugene Tan <tanyeeshuen2001@gmail.com>
2020-12-25 01:28:10 +0800

    Updated code
    fis_django:
    form.py: included placeholder text, and changed the max value for each form to the appropiate values
    
    style.css:  minor chnages to stylesheet
    
    template files(domain_ip_page.html, domain_ip_search, files_page, files_search, index):
    included the bootstrap css

--- fis_django/fastapi_table/form.py
+++ fis_django/fastapi_table/form.py
@@ -1,7 +1,23 @@
 from django import forms
 
 class DomainIpForm(forms.Form):
-    domain_ip_name = forms.CharField(label='Domain/Ip name', max_length=100)
+    domain_ip_name = forms.CharField(
+        label='Domain/Ip name',
+        max_length=255,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'e.g. google.com'
+            }
+        )
+    )
 
 class FilesForm(forms.Form):
-    file_name = forms.CharField(label='File name', max_length=100)
+    file_name = forms.CharField(
+        label='File hash',
+        max_length=64,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'SHA256 hash'
+            }
+        )
+    )
