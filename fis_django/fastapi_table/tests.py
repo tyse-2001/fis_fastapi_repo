1574384b926d85f700ab39a6690d73cd557f2906
Eugene Tan <tanyeeshuen2001@gmail.com>
2020-12-25 02:04:42 +0800

    Updated code
    - Added pycache files to gitignore
    - Added sample tests to tests.py

--- fis_django/fastapi_table/tests.py
+++ fis_django/fastapi_table/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
 
+from .models import Files, DomainIp
+
 # Create your tests here.
+class DatabaseFetchValues(TestCase):
+    def test_files_table_values_exist(self):
+        """
+        There are values in the django files model and returns True
+        """
+        entry = Files.objects.create_file(
+            "test",
+            "test",
+            "test",
+            "test",
+            "test",
+            0,
+        )
+        files_value = Files.objects.get(file_id="test")
+        self.assertEqual(files_value.file_id, "test")
+    
+    def test_domain_ip_values_exist(self):
+        """
+        There are values in the django domain_ip model and returns True
+        """
+        entry = DomainIp.objects.create_domain_ip(
+            "test",
+            "test",
+            "test",
+            "test",
+            "test",
+            0,
+            0,
+        )
+        domain_ip_value = DomainIp.objects.get(object_id="test")
+        self.assertEqual(domain_ip_value.object_id, "test")
