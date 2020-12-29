c60d80fbaadd25cbe494638048b0b35cc93cc0a8
Eugene Tan <tanyeeshuen2001@gmail.com>
2020-12-29 17:39:41 +0800

    Updated code
    fis_django:
    - Updated the Admin page to display the object fields, and show the appropiate colour for severity cell
    - Removed tests.py as it was not needed

--- fis_django/fastapi_table/admin.py
+++ fis_django/fastapi_table/admin.py
@@ -1,8 +1,36 @@
 from django.contrib import admin
 
-from .models import Files, DomainIp
+from .models import DomainIp, Files
 
 # Register your models here.
 
-admin.site.register(Files)
-admin.site.register(DomainIp)
+@admin.register(Files)
+class FilesAdmin(admin.ModelAdmin):
+    list_display = ("file_id", "file_name", "file_date_scanned", "score", "severity", "exec_parent_count")
+    list_filter = ("severity", )
+    search_fields = ("file_id", )
+    #fields = ("file_id", "severity") # Define what fields can be edited here
+
+    def suit_cell_attributes(self, obj, column):
+        if column == 'severity':
+            if getattr(obj, "severity") == "low":
+                return {'style': 'background-color: rgb(195, 247, 195);'}
+            elif getattr(obj, "severity") == "medium":
+                return {'style': 'background-color: rgb(255, 255, 175);'}
+            elif getattr(obj, "severity") == "high":
+                return {'style': 'background-color: rgb(255, 215, 222);'}
+
+@admin.register(DomainIp)
+class DomainIpAdmin(admin.ModelAdmin):
+    list_display = ("object_id", "object_type", "object_last_updated", "score", "severity", "comm_count", "ref_count")
+    list_filter = ("severity", )
+    search_fields = ("object_id", )
+
+    def suit_cell_attributes(self, obj, column):
+        if column == 'severity':
+            if getattr(obj, "severity") == "low":
+                return {'style': 'background-color: rgb(195, 247, 195);'}
+            elif getattr(obj, "severity") == "medium":
+                return {'style': 'background-color: rgb(255, 255, 175);'}
+            elif getattr(obj, "severity") == "high":
+                return {'style': 'background-color: rgb(255, 215, 222);'}
